4c3c18b1e1177d9244cfb652f4504702809a8682
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-06 22:20:58 -0500

    Update spark_session.scala

--- spark_session.scala
+++ spark_session.scala
@@ -1,8 +1,15 @@
+//Creating Spark Session
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
-object SparkSessionTest extends App {
-  val spark = SparkSession.builder()
-      .master("local[1]")
-      .appName("mysparkapp")
-      .getOrCreate();
-  println(spark)
-  println(
+
+object ParallelizeRDD {
+
+    def main(args:Array[String]) : Unit = {
+        val spark=SparkSession.builder().master("local[1]")
+          .appName("rdd_spark")
+          .getOrCreate()       
+        println(spark.version)
+    }
+
+}
+ParallelizeRDD.main(Array("spark_example"))
