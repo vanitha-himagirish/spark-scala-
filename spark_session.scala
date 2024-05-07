20f688ba7433fccd49c93fcdff93eb02b5ec0c0a
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-06 22:00:34 -0500

    creating spark session

--- spark_session.scala
+++ spark_session.scala
@@ -1,1 +1,8 @@
-
+import org.apache.spark.sql.SparkSession
+object SparkSessionTest extends App {
+  val spark = SparkSession.builder()
+      .master("local[1]")
+      .appName("mysparkapp")
+      .getOrCreate();
+  println(spark)
+  println(
