d451ebf917bbd2ead5b95f0905f527ca4a6d895c
vanitha-himagirish <55011879+vanitha-himagirish@users.noreply.github.com>
2024-05-08 12:51:42 -0500

    Update and rename repart_vs_partby (1).scala to repart_vs_partby.scala

--- optimization/repart_vs_partby.scala
+++ optimization/repart_vs_partby.scala
@@ -1,21 +1,21 @@
 // Databricks notebook source
 
 // Difference Between Repartition and Partition By
-// MAGIC
-// MAGIC Repartition : 
-// MAGIC 1. Used for In-Memory partitioning
-// MAGIC 2. Allows to control over the number of partitions
-// MAGIC 3. Useful for shuffling of the data across partitions that is useful for operations like joins and aggregations
-// MAGIC 4. Since it involves shuffling of data, it might be expensive
-// MAGIC
-// MAGIC Partition By:
-// MAGIC 1. Used for Partitioning the data on disk while writing files like parquet or ORC files that support partitioning
-// MAGIC 2. Creates subdirectories within the output directory, with each subdirectory containing data for a specific partition value.\
-// MAGIC 3. Improves query performance
-// MAGIC 4. Does not shuffle data and does not affect in memory processing
-// MAGIC
+// 
+//  Repartition : 
+//  1. Used for In-Memory partitioning
+//  2. Allows to control over the number of partitions
+//  3. Useful for shuffling of the data across partitions that is useful for operations like joins and aggregations
+//  4. Since it involves shuffling of data, it might be expensive
+// 
+//  Partition By:
+//  1. Used for Partitioning the data on disk while writing files like parquet or ORC files that support partitioning
+//  2. Creates subdirectories within the output directory, with each subdirectory containing data for a specific partition value.\
+//  3. Improves query performance
+//  4. Does not shuffle data and does not affect in memory processing
+// 
 
-// COMMAND ----------
+ ----------
 
 // Import 
 import org.apache.spark.sql.SparkSession
@@ -24,7 +24,7 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.expressions.Window
 
-// COMMAND ----------
+ ----------
 
 //Reading data
 val df=spark.read
@@ -34,46 +34,46 @@ val df=spark.read
 df.printSchema
 df.show()
 
-// COMMAND ----------
+ ----------
 
 df.rdd.getNumPartitions
 
-// COMMAND ----------
+ ----------
 
 df.count
 
-// COMMAND ----------
+ ----------
 
-// MAGIC %md
-// MAGIC Syntax Difference between repartition and partition by
+//  %md
+//  Syntax Difference between repartition and partition by
 
-// COMMAND ----------
+ ----------
 
 //Repratition based on a partition number
 df.repartition(3).rdd.getNumPartitions
 
-// COMMAND ----------
+ ----------
 
 //Repartition based on a column
 df.repartition(col("Customer_No")).rdd.getNumPartitions
 
-// COMMAND ----------
+ ----------
 
 //Repartition based on number and column
 import org.apache.spark.sql.functions.spark_partition_id
 df.repartition(3,col("Customer_No")).withColumn("PartitionID",spark_partition_id()).groupBy("PartitionID").count.show()
 
 
-// COMMAND ----------
+ ----------
 
 df.write.partitionBy("Customer_No")
 
-// COMMAND ----------
+ ----------
 
-// MAGIC %md
-// MAGIC Functional Difference
+//  %md
+//  Functional Difference
 
-// COMMAND ----------
+ ----------
 
 //In case of repartition, the partition is made in memory but while saving the disk it has not created partitions on the column
 df
@@ -83,11 +83,11 @@ df
   .mode("overwrite")
   .save("/FieStore/practice/output/customer_repart")
 
-// COMMAND ----------
+ ----------
 
-// MAGIC %fs ls "/FieStore/practice/output/customer_repart"
+//  %fs ls "/FieStore/practice/output/customer_repart"
 
-// COMMAND ----------
+ ----------
 
 //Using partition by while creating, creating partitions on the disk or storage 
 df
@@ -98,6 +98,6 @@ df
   .mode("overwrite")
   .save("/FieStore/practice/output/customer_part")
 
-// COMMAND ----------
+ ----------
 
-// MAGIC %fs ls "/FieStore/practice/output/customer_part"
+//  %fs ls "/FieStore/practice/output/customer_part"
